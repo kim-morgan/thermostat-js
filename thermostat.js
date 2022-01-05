880275aa3f35561dd73a892e921af237a20bc53b
Kimberley Morgan <kimberley.morgan135@gmail.com>
2022-01-05 17:27:24 +0000

    Full solution

--- thermostat.js
+++ thermostat.js
@@ -2,6 +2,8 @@ class Thermostat {
 
   constructor() {
     this.temperature = 20;
+    this.maxTemperature = 25;
+    this.minimumTemperature = 10;
   }
 
   getTemperature() {
@@ -9,7 +11,37 @@ class Thermostat {
   }
 
   up () {
-    this.temperature++;
+    if (this.temperature < this.maxTemperature) {
+      this.temperature++;
+    }
+  }
+
+  down () {
+    if (this.temperature > this.minimumTemperature) {
+      this.temperature--;
+    }
+  }
+
+  setPowerSavingMode(onOrOff) {
+    if (onOrOff) {
+      this.maxTemperature = 25;
+    } else if (onOrOff === false) {
+      this.maxTemperature = 32;
+    }
+  }
+
+  reset() {
+    this.temperature = 20;
+  }
+
+  checkEnergyUsage() {
+    if (this.temperature <= 18) {
+      return "Low";
+    } else if (this.temperature <=25) {
+      return "Medium";
+    } else {
+      return "High";
+    }
   }
 
 }
--- thermostat.test.js
+++ thermostat.test.js
@@ -11,4 +11,47 @@ describe("thermostat", () => {
     thermostat.up();
     expect(thermostat.getTemperature()).toBe(22);
   })
+  it("should be able to decrease the temperature", () => {
+    thermostat.down();
+    expect(thermostat.getTemperature()).toBe(21);
+  })
+  it("should have a default max temperature of 25", () => {
+    for (let i = 0 ; i < 10 ; i++) {
+      thermostat.up();
+    }
+    expect(thermostat.getTemperature()).toBe(25);
+  })
+  it("can go up to 32 if power saving mode is off", () => {
+    thermostat.setPowerSavingMode(false);
+    for (let i = 0 ; i < 20 ; i++) {
+      thermostat.up();
+    }
+    expect(thermostat.getTemperature()).toBe(32);
+  })
+  it("can reset the temperature to 20", () => {
+    thermostat.reset();
+    expect(thermostat.getTemperature()).toBe(20);
+  })
+  it("can check the current usage (medium)", () => {
+    expect(thermostat.checkEnergyUsage()).toBe("Medium");
+  })
+  it("can check the current usage (high)", () => {
+    for (let i = 0 ; i < 20 ; i++) {
+      thermostat.up();
+    }
+    expect(thermostat.checkEnergyUsage()).toBe("High");
+  })
+  it("can check the current usage (low)", () => {
+    thermostat.reset();
+    thermostat.down();
+    thermostat.down();
+    expect(thermostat.checkEnergyUsage()).toBe("Low");
+  })
+
+  it('should have a minimum temp of 10', () => {
+    for (let i = 0 ; i < 20 ; i++) {
+      thermostat.down();
+    }
+    expect(thermostat.getTemperature()).toBe(10);
+  })
 })
